840a8ddc5411954bb8f6333e31d648924f933a83
QEDK <1994constant@gmail.com>
2020-09-26 14:33:22 +0530

    Add dummy signin endpoint

--- app/app/views.py
+++ app/app/views.py
@@ -1,5 +1,23 @@
-from django.http import HttpResponse
-
+from http import HTTPStatus
+import json
+import pymongo
+from django.http import HttpResponse, HttpResponseNotAllowed
+from passlib.hash import pbkdf2_sha256
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+def signin(request):
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+    else:
+        try:
+            credentials = json.loads(request.body.decode('utf-8'))
+            validate(credentials['uid'], credentials['password'])
+            return HttpResponse()
+        except:
+            return HttpResponse(status=HTTPStatus.BAD_REQUEST)
+
+def validate(uid, password):
+    if uid == 'admin' and password == 'password':
+        pass

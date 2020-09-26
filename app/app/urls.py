7e111e9990ece1ee7f9fcbc60d2695b5327e02ce
QEDK <1994constant@gmail.com>
2020-09-26 21:53:28 +0530

    Add routing for signup

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,6 +21,8 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('signin', views.signin, name='signin'),
     path('signin/', views.signin, name='signin'),
-    path('signin', views.signin, name='signin')
+    path('signup', views.signup, name='signup'),
+    path('signup/', views.signup, name='signup')
 ]

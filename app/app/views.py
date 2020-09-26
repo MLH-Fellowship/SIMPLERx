3108cfce25777fa2a026919cf9b251f2ca9c3da6
QEDK <1994constant@gmail.com>
2020-09-26 19:17:47 +0530

    Add dummy authentication

--- app/app/views.py
+++ app/app/views.py
@@ -13,11 +13,30 @@ def signin(request):
     else:
         try:
             credentials = json.loads(request.body.decode('utf-8'))
+            if validate(credentials['uid'], credentials['password']):
+                response = HttpResponse(status=HTTPStatus.OK)
+                response.set_signed_cookie('uid', credentials['uid'], max_age=30*24*60*60)
+                response.set_signed_cookie('loggedin', 'true', salt=credentials['uid'], max_age=30*24*60*60)
+            else:
+                response = HttpResponse(status=HTTPStatus.UNAUTHORIZED)
+            return response
+        except:
+            return HttpResponse(status=HTTPStatus.BAD_REQUEST)
+
+def signup(request):
+    if request.method != 'POST':
+        return HttpResponseNotAllowed(['POST'])
+    else:
+        try:
+            credentials = json.loads(request.body.decode('utf-8'))
             validate(credentials['uid'], credentials['password'])
             return HttpResponse()
         except:
             return HttpResponse(status=HTTPStatus.BAD_REQUEST)
 
+
 def validate(uid, password):
     if uid == 'admin' and password == 'password':
-        pass
+        return True
+    else:
+        return False
